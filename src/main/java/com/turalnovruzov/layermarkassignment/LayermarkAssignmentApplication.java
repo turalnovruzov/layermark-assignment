1b60b4db67f88e61fb9217b74c618a105bbe068f
Tural Novruzov <tural.nt.1@gmail.com>
2022-04-13 14:37:59 +0300

    Created the project with Spring Initializr.

---
+++ src/main/java/com/turalnovruzov/layermarkassignment/LayermarkAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.turalnovruzov.layermarkassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayermarkAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayermarkAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turalnovruzov/layermarkassignment/LayermarkAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turalnovruzov.layermarkassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayermarkAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
