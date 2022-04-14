0dfe00ef3523bf9a5a28251bc0f2b1135e95db3a
Tural Novruzov <tural.nt.1@gmail.com>
2022-04-14 21:43:47 +0300

    Added BookController and BookService and implemented getAllBooks

---
+++ src/main/java/com/turalnovruzov/layermarkassignment/book/BookService.java
@@ -0,0 +1,17 @@
+package com.turalnovruzov.layermarkassignment.book;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
