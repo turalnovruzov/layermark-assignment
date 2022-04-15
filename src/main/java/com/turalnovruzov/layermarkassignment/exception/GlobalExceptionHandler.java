78133aea96192701d97ea8c49b76888a2b8cb3b5
Tural Novruzov <tural.nt.1@gmail.com>
2022-04-15 14:05:29 +0300

    Edited GlobalExceptionHandler to be more similar to the default error message

--- src/main/java/com/turalnovruzov/layermarkassignment/exception/GlobalExceptionHandler.java
+++ src/main/java/com/turalnovruzov/layermarkassignment/exception/GlobalExceptionHandler.java
@@ -1,17 +1,23 @@
 package com.turalnovruzov.layermarkassignment.exception;
 
 import com.turalnovruzov.layermarkassignment.payload.ErrorResponse;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse resourceNotFoundHandler(ResourceNotFoundException ex) {
-        return new ErrorResponse("Not found.");
+    @ExceptionHandler({ResourceNotFoundException.class, EmptyResultDataAccessException.class})
+    public ErrorResponse resourceNotFoundException(HttpServletRequest request) {
+        return new ErrorResponse(
+                new Date(),
+                HttpStatus.NOT_FOUND.value(),
+                "Resource not found.",
+                request.getRequestURI());
     }
 }
--- src/main/java/com/turalnovruzov/layermarkassignment/payload/ErrorResponse.java
+++ src/main/java/com/turalnovruzov/layermarkassignment/payload/ErrorResponse.java
@@ -1,17 +1,49 @@
 package com.turalnovruzov.layermarkassignment.payload;
 
+import java.util.Date;
+
 public class ErrorResponse {
-    private String message;
+    private Date timestamp;
+    private int statusCode;
+    private String error;
+    private String path;
 
-    public ErrorResponse(String message) {
-        this.message = message;
+    public ErrorResponse(Date timestamp, int statusCode, String error, String path) {
+        this.timestamp = timestamp;
+        this.statusCode = statusCode;
+        this.error = error;
+        this.path = path;
     }
 
-    public String getMessage() {
-        return message;
+    public Date getTimestamp() {
+        return timestamp;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
     }
 }
